bb7c05fe90433012b9ddb9579756ef5624f1dd3f
Mateo0o0o <matthewmanalo111@gmail.com>
2024-12-04 21:42:23 +0800

    1st and 2nd Derivative

--- PythOptimizer.py
+++ PythOptimizer.py
@@ -1,4 +1,25 @@
 import sympy as sm
+from sympy.abc import x
 import tabulate as tab
 
+# Parameters used
+h = 0.001
+max_iter = 200
+tol = 1e-20 
+
+# Inputs
 funct = sm.sympify(input("Enter the function: "))
+vrbl = sm.symbols(input("Enter the variable used: "))
+guess = float(input("Initial Guess: "))
+
+# 1st Derivative
+term1 = funct.subs(vrbl, vrbl-2*h)
+term2 = funct.subs(vrbl, vrbl-h)
+term3 = funct.subs(vrbl, vrbl+h)
+term4 = funct.subs(vrbl, vrbl+2*h)
+derv1 = (term1 -8*term2 + 8*term3 - term4)/(12*h)
+
+# 2nd Derivative
+derv2 = (-term1 + 16*term2 - 30*funct + 16*term3 - term4)/(12*h**2)
+print(derv1)
+print(derv2)
