918ac06f8e9858a7da63282cb34e5631290cb962
Mateo0o0o <matthewmanalo111@gmail.com>
2024-12-05 19:02:51 +0800

    Reverted back by 1 version. The solution made for linear and constant polynomials causes error for transcendental functions.

--- PythOptimizer.py
+++ PythOptimizer.py
@@ -11,7 +11,6 @@ tol = 1e-20
 funct = sm.sympify(input("Enter the function: "))
 vrbl = sm.symbols(input("Enter the variable used: "))
 guess = float(input("Initial Guess: "))
-degree = sm.degree(funct)
 
 # 1st Derivative
 term1 = funct.subs(vrbl, vrbl-2*h)
@@ -29,29 +28,25 @@ data = []
 params = {"xprev": guess, "fxprev": 1, "xnew":1, "fxnew":1, "apprx_error":1}
 params["xprev"] = guess - (derv1.subs(vrbl, guess)/derv2.subs(vrbl, guess))
 
-if degree != 0 and degree != 1:
-    while i <= max_iter and params["apprx_error"] > tol:
-        i += 1
-        params["xnew"] = params["xprev"] - (derv1.subs(vrbl, params["xprev"])/derv2.subs(vrbl, params["xprev"]))
-        params["fxnew"] = derv1.subs(vrbl,  params["xnew"])
-        if params["xnew"] == 0:
-            params["apprx_error"] = 0
-        else:
-            params["apprx_error"] = abs((params["xnew"]-params["xprev"]))/abs(params["xnew"])
-        params["xprev"] = params["xnew"]
-        params["fxprev"] = derv1.subs(vrbl,  params["xprev"])
+while i <= max_iter and params["apprx_error"] > tol:
+    i += 1
+    params["xnew"] = params["xprev"] - (derv1.subs(vrbl, params["xprev"])/derv2.subs(vrbl, params["xprev"]))
+    params["fxnew"] = derv1.subs(vrbl,  params["xnew"])
+    if params["xnew"] == 0:
+        params["apprx_error"] = 0
+    else:
+        params["apprx_error"] = abs((params["xnew"]-params["xprev"]))/abs(params["xnew"])
+    params["xprev"] = params["xnew"]
+    params["fxprev"] = derv1.subs(vrbl,  params["xprev"])
 
-        data.append(params.copy().values())
+    data.append(params.copy().values())
 
-    # Tests for Maxima or Minima
-    if derv2.subs(vrbl, params["xnew"]) > 0:
-        max_min = "Minima"
-    elif derv2.subs(vrbl, params["xnew"]) < 0:
-        max_min = "Maxima"
+# Tests for Maxima or Minima
+if derv2.subs(vrbl, params["xnew"]) > 0:
+    max_min = "Minima"
+elif derv2.subs(vrbl, params["xnew"]) < 0:
+    max_min = "Maxima"
 
-    # print the data in tabular form and final answer in 15 decimal digit precision
-    print("\n" + tabulate.tabulate(data, params.keys(), "double_outline", showindex=True,  colalign = ("center", "center", "center" , "center", "center", "center")) + "\n")
-    print(tabulate.tabulate([[params["xnew"], params["apprx_error"], max_min ]], ["Critical Value (x)", "Approximation Error", "Maxima/Minima"], "fancy_grid",floatfmt=[".15f"], colalign = ("center", "center", "center")), "\n")
-
-else:
-    print("The function has no critical value!")
+# print the data in tabular form and final answer in 15 decimal digit precision
+print("\n" + tabulate.tabulate(data, params.keys(), "double_outline", showindex=True,  colalign = ("center", "center", "center" , "center", "center", "center")) + "\n")
+print(tabulate.tabulate([[params["xnew"], params["apprx_error"], max_min ]], ["Critical Value (x)", "Approximation Error", "Maxima/Minima"], "fancy_grid",floatfmt=[".15f"], colalign = ("center", "center", "center")), "\n")
